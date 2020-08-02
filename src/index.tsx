646258349131e5e4323810cbdc275582e73a1a1a
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-08-02 21:09:18 +0300

    Reverse timeline

--- src/index.tsx
+++ src/index.tsx
@@ -61,7 +61,7 @@ const Day = ({ date, offset }: { date: Date; offset: number }) => {
   );
   const timetable = React.useMemo(
     () => [
-      ['00 am', '1 am', '2 am', '3 am', '4 am', '5 am'],
+      ['0 am', '1 am', '2 am', '3 am', '4 am', '5 am'],
       ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am'],
       ['12 pm', '13 pm', '14 pm', '15 pm', '16 pm', '17 pm'],
       ['18 pm', '19 pm', '20 pm', '21 pm', '22 pm', '23 pm'],
@@ -157,7 +157,7 @@ const App = () => {
                 name="calendar"
                 onValuesChange={handleSaveFormDataToLs}
               >
-                <Timeline mode="left">
+                <Timeline mode="left" reverse>
                   {daysToRender.map((_, idx) => (
                     <Day date={startDate} offset={idx} key={idx} />
                   ))}
